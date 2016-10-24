2b89a455f23208410a6a75ec0221b166dc946f80
1ats <>
2016-10-24 02:39:59 +0100

    meu primeiro commit

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -6,8 +6,10 @@ public class Git {
     /**
      * @param args the command line arguments
      */
+    String tuti;
     public static void main(String[] args) {
         System.out.println("alberto Tuti Soki");
+        
     }
     
 }
