a2b5c2e8ef4f531bd077a355995fdc4b497621a4
HP <HP@HP-PC>
2016-10-24 02:36:05 +0100

    meu primeiro commit

---
+++ Git/src/git/Git.java
@@ -0,0 +1,13 @@
+
+package git;
+
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("alberto Tuti Soki");
+    }
+    
+}
